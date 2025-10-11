9c3131ad6a9a32c2d3954d76e0d942194511fc8a
EmilXavierVT <emilxavierthorsen@gmail.com>
2025-10-11 11:02:03 +0200

    initial setup

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,44 @@
-package app;
+ package app;
 
-public class Main {
-}
+import app.config.ThymeleafConfig;
+import app.controllers.UserController;
+import app.entities.User;
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+    public class Main
+    {
+        private static final String USER = "postgres";
+        private static final String PASSWORD = "postgres";
+        private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+        private static final String DB = "morgendagens_maaltid";
+
+        private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+
+        public static void main(String[] args)
+        {
+            // Initializing Javalin and Jetty webserver
+            Javalin app = Javalin.create(config -> {
+                config.staticFiles.add("/public");
+                config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+                config.staticFiles.add("/templates");
+            }).start(7070);
+
+
+            // Routing
+            app.get("/", ctx -> ctx.render("index.html"));
+
+            app.before(ctx -> {
+                User currentUser = ctx.sessionAttribute("currentUser");
+                if (currentUser != null) {
+                    ctx.attribute("user", currentUser);
+                }
+            });
+            UserController.addRoutes(app);
+
+        }
+    }
+
+
