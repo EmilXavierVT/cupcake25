071ad79b8cfb5c8a28d0ebae51347ed948c9a2d1
frede <frededv@outlook.dk>
2025-10-28 10:31:07 +0000

    Solved merge conflict,

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -11,7 +11,7 @@ import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
     public class Main
-    {
+    {  
         private static final String USER = "postgres";
         private static final String PASSWORD = "postgres";
         private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
